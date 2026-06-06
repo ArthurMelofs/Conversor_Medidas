c3e1957ebedea3d9d8fbea541955ee468841ab3c
Arthur Melo <meloarthurfs@gmail.com>
2026-06-05 21:16:49 -0300

    implementação do html

--- Conversor.py
+++ Conversor.py
@@ -1,124 +1,54 @@
-#Função que faz a conversão celsius para fahrenheit#
-def temperatura():
+from flask import Flask, render_template, request
 
-  print("1 - Celsius para Fahrenheit")
-  print("2 - Fahrenheit para Celsius")
+app = Flask(__name__)
 
-  opcao = int(input("Escolha uma das opções: "))
+@app.route("/", methods=["GET", "POST"])
+def index():
 
-  if opcao == 1: 
-    c = float (input("Digite a temperatura em Celsius: "))
-    f = (c * 9/5) + 32
-    print("Fahrenheit: ", round(f, 2) )
- 
-  elif opcao == 2:
-    f = float (input("Digite a temperatura em Fahrenheit: "))
-    c = (f - 32) * 5/9
-    print("Celsius: ", round(c, 2) )
-  
-  else:
-    print("Opção inválida")
+    resultado = None
 
-#Função que faz a conversão de metros para pés#
-def metros():
+    if request.method == "POST":
 
-  print("1 - Metros para Pés")
-  print("2 - Pés para Metros")
+        tipo = request.form["tipo"]
+        valor = float(request.form["valor"])
 
-  opcao = int(input("Escolha uma das opções: "))
+        # Temperatura
+        if tipo == "c_f":
+            resultado = (valor * 9/5) + 32
+            resultado = f"{round(resultado, 2)} °F"
 
-  if opcao == 1:
-    m = float(input("Digite o valor em metros: "))
-    p = m * 3.280
-    print("Pés: ", round(p, 2))
-  
-  elif opcao == 2:
-    p = float(input("Digite o valor em pés: "))
-    m = p / 3.280
-    print("Metros: ", round(m, 2))
-  
-  else:
-    print("Opção inválida")
+        elif tipo == "f_c":
+            resultado = (valor - 32) * 5/9
+            resultado = f"{round(resultado, 2)} °C"
 
-#Função que faz a conversão de Quilos para libras#
-def quilos():
+        # Metros e pés
+        elif tipo == "m_p":
+            resultado = valor * 3.280
+            resultado = f"{round(resultado, 2)} pés"
 
-  print("1 - Quilos para Libras")
-  print("2 - Libras para quilos")
+        elif tipo == "p_m":
+            resultado = valor / 3.280
+            resultado = f"{round(resultado, 2)} metros"
 
-  opcao = int(input("Escolha uma das opções: "))
+        # Quilos e libras
+        elif tipo == "kg_lb":
+            resultado = valor * 2.20
+            resultado = f"{round(resultado, 2)} libras"
 
-  if opcao == 1:
-    kg = float(input("Digite o valor em quilos(Kg): "))
-    lb = kg * 2.20
-    print("Libras: ", round(lb, 2))
+        elif tipo == "lb_kg":
+            resultado = valor / 2.20
+            resultado = f"{round(resultado, 2)} kg"
 
-  elif opcao == 2:
-    lb = float(input("Digite o valor em libras: "))
-    kg = lb / 2.20
-    print("Quilos: ", round(kg, 2))
-  
-  else:
-    print("Opção inválida")
+        # Quilômetros e milhas
+        elif tipo == "km_ml":
+            resultado = valor * 0.621
+            resultado = f"{round(resultado, 2)} milhas"
 
-#Função que faz a conversão de quilometros para milhas#
-def quilometros():
-    
-    print("1 - Quilometros ppara Milhas")
-    print("2 - Milhas para Quilometros")
+        elif tipo == "ml_km":
+            resultado = valor / 0.621
+            resultado = f"{round(resultado, 2)} km"
 
-    opcao = int(input("Escolha uma das opções: "))
+    return render_template("index.html", resultado=resultado)
 
-    if opcao == 1:
-      km = float(input("Digite o valor em quilometros(Km): "))
-      ml = km * 0.621
-      print("Milhas: ", round (ml, 2))
-
-    elif opcao == 2:
-      ml = float(input("Digite o valor em milhas: "))
-      km = ml / 0.621
-      print("Quilometros: ", round(km, 2))
-    
-    else:
-      print("Opção inválida")
-#Menu pnde o usuário pode escolher o que quer converter #
-def menu():
-  print("=== Conversor de unidades ===")
-
-  print("1 - Temperatura")
-  print("2 - Metros")
-  print("3 - Quilos")
-  print("4 - Quilômetros")
-  print("5 - Sair")
-
-  opcao = int(input("Escolha uma das opções: "))
-
-  if opcao == 1:
-      temperatura()
-  elif opcao == 2:
-        metros()
-  elif opcao == 3:
-        quilos()
-  elif opcao == 4:
-      quilometros()
-  elif opcao == 5:
-    print("Encerrando programa")
-    exit()
-  else:
-        print("Opção inválida")
-
-#laço para fazer o usuário retornar a tela de opções#
-while True:
-    menu()
-
-    # loop de validação
-    while True:
-        resposta = input("Deseja fazer outra conversão? (s/n): ").lower()
-
-        if resposta == 's':
-            break
-        elif resposta == 'n':
-            print("Encerrando programa...")
-            exit()
-        else:
-            print("Opção inválida! Digite apenas 's' ou 'n'")
+if __name__ == "__main__":
+    app.run(debug=True)
